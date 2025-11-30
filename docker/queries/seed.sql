c90d1465c9f3f8d7f889dee8243ca48c9823a46f
Lucas-DaCosta <lucas.dacosta@edu.ecole-89.com>
2025-11-30 18:29:42 +0100

    changed seed.sql

--- docker/queries/seed.sql
+++ docker/queries/seed.sql
@@ -1,7 +1,7 @@
 -- connect the seed : docker exec -i docker-db-1 psql -U lucasDC -d lucasDC -f queries/seed.sql
 
 INSERT INTO users (username, speciality, description, avatar) VALUES
-('Administrator', 'admin', 'I am the admin', ''),
+('Dony  Boubzi', 'marketing digital', 'Je vous apprend à arnaquer les gens', 'https://yt3.googleusercontent.com/ytc/AIdro_kgViksrHa4dXWFP98GqdT4oGHZR4J4moqgTzyCcP3sOyQ=s900-c-k-c0x00ffffff-no-rj'),
 ('John Doe', 'dev front', 'Je code en font des applications web avec react et angular', ''),
 ('Jane Smith', 'dev back', 'Je code des API back-end pour des applications web', ''),
 ('Arthur Morgan', 'dev full stack', 'Je créé des applications web de A à Z', 'https://imgs.search.brave.com/UHkb6F5dO25oTgmYYVE-SQ2flCzb80-_-44qwW1VPfU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/aS1zd2Vhci1hcnRo/dXItbW9yZ2FuLWlz/LW9uZS1vZi10aGUt/ZmV3LWdhbWUtY2hh/cmFjdGVycy12MC1u/bzU2N2F6cnNkeGUx/LmpwZWc_d2lkdGg9/NjQwJmNyb3A9c21h/cnQmYXV0bz13ZWJw/JnM9YzczYmEzOTI1/NmI3ZDlkMmZiZTRl/YWFiZDY4MzkxMGIw/YjFjNGRhMw');
@@ -19,7 +19,7 @@ INSERT INTO timeslots (start_date, end_date, username, user_email, id_user) VALU
 ('2025-11-20 09:00:00', '2025-11-21 16:00:00', 'Steve', 'steve@mojang.com', 3);
 
 INSERT INTO linktree (name, logo, url, id_user) VALUES
--- Administrator
+-- Dony
 ('LinkedIn professionnel', 'linkedin', 'https://www.linkedin.com/in/administrator', 1),
 ('GitHub - Scripts Admin', 'github', 'https://github.com/administrator/admin-scripts', 1),
 
